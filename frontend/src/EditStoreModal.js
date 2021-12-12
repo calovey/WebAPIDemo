dca08cfb632e01a633fd6ad9f2a80ddf20d6ba26
Damla VARAVIR <d_varavir@hotmail.com>
2021-12-12 14:26:17 +0300

    frontend: src: Store.js: Edit modal added

---
+++ frontend/src/EditStoreModal.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { Modal, Button, Row, Col, Form } from 'react-bootstrap';
+import 'react-responsive-modal/styles.css';
+export class EditStoreModal extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        fetch(process.env.REACT_APP_API + 'Store', {
+            method: 'PUT',
+            headers: {
+                'Accept': '*/*',
+                'Content-Type': 'application/json',
+                'Accept-Encoding': 'gzip, deflate, br'
+            },
+            //mode: 'no-cors',
+            body: JSON.stringify({
+                StoreId: event.target.StoreId.value,
+                StoreName: event.target.StoreName.value
+            })
+        })
+            .then(res => res.json())
+            .then((data) => {
+                alert(data);
+            },
+                (error) => {
+                    alert('Failed');
+                })
+    }
+    render() {
+        return (
+            <div className="container">
+
+                <Modal
+                    {...this.props}
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                >
+                    <Modal.Header>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            Edit Store
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+
+                        <Row>
+                            <Col sm={6}>
+                                <Form onSubmit={this.handleSubmit}>
+                                    <Form.Group controlId="StoreId">
+                                        <Form.Label>Store Id</Form.Label>
+                                        <Form.Control type="text" name="StoreId" required
+                                            disabled
+                                            defaultValue={this.props.storeid}
+                                            placeholder="Store ID" />
+                                    </Form.Group>
+
+                                    <Form.Group controlId="StoreName">
+                                        <Form.Label>Store Name</Form.Label>
+                                        <Form.Control type="text" name="StoreName" required
+                                            defaultValue={this.props.storename}
+                                            placeholder="Store Name" />
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Button variant="primary" type="submit">
+                                            Update Store
+                                        </Button>
+                                    </Form.Group>
+                                </Form>
+                            </Col>
+                        </Row>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button variant="danger" onClick={this.props.onHide}>Close</Button>
+                    </Modal.Footer>
+
+                </Modal>
+
+            </div>
+        )
+    }
+
+}
--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -3,6 +3,7 @@ import { Table } from 'react-bootstrap';
 import { Button, ButtonToolbar } from 'react-bootstrap';
 import 'react-responsive-modal/styles.css';
 import { AddStoreModal } from './AddStoreModal';
+import { EditStoreModal } from './EditStoreModal';
 export class Store extends Component {
 
     constructor(props) {
@@ -46,9 +47,21 @@ export class Store extends Component {
                                 <td>{str.StoreName}</td>
                                 <td>
                                     <ButtonToolbar>
-                                        <Button variant="success" >
-                                            Update
+                                        <Button className="btn btn-success"
+                                            onClick={() => this.setState({
+                                                editModalShow: true,
+                                                storeid: str.StoreId, storename: str.StoreName
+                                            })}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-vector-pen" viewBox="0 0 16 16">
+                                                <path fillRule="evenodd" d="M10.646.646a.5.5 0 0 1 .708 0l4 4a.5.5 0 0 1 0 .708l-1.902 1.902-.829 3.313a1.5 1.5 0 0 1-1.024 1.073L1.254 14.746 4.358 4.4A1.5 1.5 0 0 1 5.43 3.377l3.313-.828L10.646.646zm-1.8 2.908-3.173.793a.5.5 0 0 0-.358.342l-2.57 8.565 8.567-2.57a.5.5 0 0 0 .34-.357l.794-3.174-3.6-3.6z" />
+                                                <path fillRule="evenodd" d="M2.832 13.228 8 9a1 1 0 1 0-1-1l-4.228 5.168-.026.086.086-.026z" />
+                                            </svg>
+                                            Edit
                                         </Button>
+                                        <EditStoreModal show={this.state.editModalShow}
+                                            onHide={editModalClose}
+                                            storeid={storeid}
+                                            storename={storename} />
                                         <AddStoreModal show={this.state.addModalShow}
                                             onHide={addModalClose}></AddStoreModal>
                                     </ButtonToolbar>
@@ -60,15 +73,14 @@ export class Store extends Component {
                 <ButtonToolbar>
                     <Button className="btn btn-primary"
                         onClick={() => this.setState({ addModalShow: true })}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
                         </svg>
                         New Store
                     </Button>
                     <AddStoreModal show={this.state.addModalShow}
                         onHide={addModalClose}></AddStoreModal>
                 </ButtonToolbar>
-
             </div>
         )
     }
